c969b85c05d6e82c67e1dd4fecfe648e97615208
Oliver Fleckenstein <oliverfl@live.dk>
2017-04-07 15:48:30 +0200

    implemented facility

--- src/model/Facility.java
+++ src/model/Facility.java
@@ -2,6 +2,30 @@ package model;
 
 public abstract class Facility {
 
-	private int lan;
-	private int lon;
+	private String name;
+	
+	private double longitude; 
+	private double latitude;
+	
+	protected Facility(String name, double longitude, double latitude)
+	{
+		this.name = name;
+		this.longitude = longitude;
+		this.latitude = latitude;
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public double getLongitude()
+	{
+		return this.longitude;
+	}
+	
+	public double getLatitue()
+	{
+		return this.latitude;
+	}
 }
---
+++ src/model/ResouceNode.java
@@ -0,0 +1,5 @@
+package model;
+
+public class ResouceNode extends Facility {
+
+}
