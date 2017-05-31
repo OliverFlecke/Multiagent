9ac6482efa1f2eb35062de35fff7f7a38a9ad78c
Oliver Fleckenstein <oliverfl@live.dk>
2017-05-31 15:43:36 +0200

    Fixed internal action so it converts correctly

--- src/jia/action.java
+++ src/jia/action.java
@@ -6,17 +6,17 @@ import env.Translator;
 import jason.asSemantics.DefaultInternalAction;
 import jason.asSemantics.TransitionSystem;
 import jason.asSemantics.Unifier;
-import jason.asSyntax.LiteralImpl;
 import jason.asSyntax.Term;
 
 public class action extends DefaultInternalAction {
 
-	
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	public Object execute(TransitionSystem ts, Unifier un, Term[] terms) throws Exception {
 		
 		String agentName = (String) Translator.termToObject(terms[0]);
-		Action action = Translator.literalToAction((LiteralImpl) terms[1]);
+		Action action = Translator.termToAction(terms[1]);
 
 		EIArtifact.executeAction(agentName, action);
 		
