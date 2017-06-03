28b2b6c5686b790863bafcb01c5f661721d1cf5e
Oliver Fleckenstein <oliverfl@live.dk>
2017-06-03 11:21:04 +0200

    Added functions for getting a shopping list

--- src/data/CUtil.java
+++ src/data/CUtil.java
@@ -1,5 +1,6 @@
 package data;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
@@ -18,6 +19,21 @@ public class CUtil {
 	public static Map<String, Integer> toStringMap(Map<Item, Integer> items) {
 		return items.entrySet().stream()
 				.collect(Collectors.toMap(e -> e.getKey().getName(), Entry::getValue));
+	}
+	
+	/**
+	 * @param map The map to add the content to
+	 * @param first The element in the first map, which the content should be added to 
+	 * @param second The element in the second map, to which the content should be addad
+	 * @param content The content to add
+	 */
+	public static <A,B,C> void addToMapOfMaps(Map<A, Map<B, C>> map, A first, B second, C content) 
+	{
+		if (!map.containsKey(first))
+		{
+			map.put(first, new HashMap<B, C>());
+		}
+		map.get(first).put(second, content);
 	}
 
 }
